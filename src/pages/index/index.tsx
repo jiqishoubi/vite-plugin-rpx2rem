c7ccce0b472ec0e03bc551c01d45e515f45326c3
jiqishoubi <437753880@qq.com>
2023-10-11 17:39:01 +0800

    1

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { router } from 'kuririn-react-router'
+import styles from './index.module.less'
 
 const Index: React.FC = () => {
   return (
     <>
       <h1>index page</h1>
+
+      <div className={styles.red}>test css modules</div>
 
       <button
         onClick={() => {
